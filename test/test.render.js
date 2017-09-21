cc0e85c313400e9d573e3ce645e941e5cc03e1df
Greg Allen <greg@firstandthird.com>
2017-09-20 17:01:09 -0700

    added support for common object to include common data

--- test/test.render.js
+++ test/test.render.js
@@ -16,6 +16,91 @@ tap.test('can initiate the class', (t) => {
   t.end();
 });
 
+tap.test('fetch', (t) => {
+  const pr = new PagedataRenderer('apiKey', {
+    path: __dirname,
+    host: 'http://localhost:8081'
+  });
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({
+        host: 'localhost',
+        port: 8081
+      });
+      server.route({
+        path: '/api/pages/page-slug',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { text: 'Hello World' } });
+        }
+      });
+      server.start(() => done(null, server));
+    },
+    call(server, done) {
+      pr.fetch('page-slug', (err, result) => {
+        t.equal(err, null);
+        t.deepEqual(result, { content: { text: 'Hello World' } });
+        done();
+      });
+    },
+    stop(server, call, done) {
+      server.stop(done);
+    }
+  }, t.end);
+});
+
+tap.test('fetch with common', (t) => {
+  const pr = new PagedataRenderer('apiKey', {
+    path: __dirname,
+    host: 'http://localhost:8081',
+    common: {
+      header: 'site-header'
+    }
+  });
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({
+        host: 'localhost',
+        port: 8081
+      });
+      server.route({
+        path: '/api/pages/page-slug',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { text: 'Hello World' } });
+        }
+      });
+      server.route({
+        path: '/api/pages/site-header',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { logo: 'awesome' } });
+        }
+      });
+      server.start(() => done(null, server));
+    },
+    call(server, done) {
+      pr.fetch('page-slug', (err, result) => {
+        t.equal(err, null);
+        t.deepEqual(result, {
+          header: {
+            logo: 'awesome'
+          },
+          content: {
+            text: 'Hello World'
+          }
+        });
+        done();
+      });
+    },
+    stop(server, call, done) {
+      server.stop(done);
+    }
+  }, t.end);
+});
+
 tap.test('render', (t) => {
   const pr = new PagedataRenderer('apiKey', { path: __dirname });
   pr.render(templateFile, { content: { text: 'Hello World' } }, (err, result) => {
@@ -56,6 +141,7 @@ tap.test('renderPage', (t) => {
   }, t.end);
 });
 
+
 tap.test('renderCollection', (t) => {
   const pr = new PagedataRenderer('apiKey', { path: __dirname, host: 'http://localhost:8081' });
   async.autoInject({
@@ -84,6 +170,58 @@ tap.test('renderCollection', (t) => {
         t.equal(err, null);
         t.notEqual(result['page1-slug'].indexOf('Hello World 1'), -1);
         t.notEqual(result['page2-slug'].indexOf('Hello World 2'), -1);
+        done();
+      });
+    },
+    stop(server, call, done) {
+      server.stop(done);
+    }
+  }, t.end);
+});
+
+
+tap.test('renderCollection with common', (t) => {
+  const templateFile2 = path.join(__dirname, 'fixture', 'commonPage.njk');
+  const pr = new PagedataRenderer('apiKey', {
+    path: __dirname,
+    host: 'http://localhost:8081',
+    common: {
+      header: 'website-header'
+    }
+  });
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({
+        host: 'localhost',
+        port: 8081
+      });
+      server.route({
+        path: '/api/pages',
+        method: 'get',
+        handler(request, reply) {
+          t.equal(request.query.parentPageSlug, 'collection-slug');
+          t.equal(request.query.populate, 'content');
+          return reply(null, [
+            { slug: 'page1-slug', content: { text: 'Hello World 1' } },
+            { slug: 'page2-slug', content: { text: 'Hello World 2' } },
+          ]);
+        }
+      });
+      server.route({
+        path: '/api/pages/website-header',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { logo: 'awesome' } });
+        }
+      });
+      server.start(() => done(null, server));
+    },
+    call(server, done) {
+      pr.renderCollection('collection-slug', templateFile2, (err, result) => {
+        t.equal(err, null);
+        t.notEqual(result['page1-slug'].indexOf('awesomeHello World 1'), -1);
+        t.notEqual(result['page2-slug'].indexOf('awesomeHello World 2'), -1);
         done();
       });
     },
