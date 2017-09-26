79f85b24d66c3ba3294293844114e7a45ae4d1af
orthagonal <orthagonal1@gmail.com>
2017-09-26 17:53:10 -0500

    merge tests

--- test/test.render.js
+++ test/test.render.js
@@ -17,6 +17,91 @@ tap.test('can initiate the class', (t) => {
   t.end();
 });
 
+tap.test('fetch', (t) => {
+  const pr = new PagedataRenderer('apiKey', {
+    path: __dirname,
+    host: 'http://localhost:8081'
+  });
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({
+        host: 'localhost',
+        port: 8081
+      });
+      server.route({
+        path: '/api/pages/page-slug',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { text: 'Hello World' } });
+        }
+      });
+      server.start(() => done(null, server));
+    },
+    call(server, done) {
+      pr.fetch('page-slug', (err, result) => {
+        t.equal(err, null);
+        t.deepEqual(result, { content: { text: 'Hello World' } });
+        done();
+      });
+    },
+    stop(server, call, done) {
+      server.stop(done);
+    }
+  }, t.end);
+});
+
+tap.test('fetch with common', (t) => {
+  const pr = new PagedataRenderer('apiKey', {
+    path: __dirname,
+    host: 'http://localhost:8081',
+    common: {
+      header: 'site-header'
+    }
+  });
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({
+        host: 'localhost',
+        port: 8081
+      });
+      server.route({
+        path: '/api/pages/page-slug',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { text: 'Hello World' } });
+        }
+      });
+      server.route({
+        path: '/api/pages/site-header',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { logo: 'awesome' } });
+        }
+      });
+      server.start(() => done(null, server));
+    },
+    call(server, done) {
+      pr.fetch('page-slug', (err, result) => {
+        t.equal(err, null);
+        t.deepEqual(result, {
+          header: {
+            logo: 'awesome'
+          },
+          content: {
+            text: 'Hello World'
+          }
+        });
+        done();
+      });
+    },
+    stop(server, call, done) {
+      server.stop(done);
+    }
+  }, t.end);
+});
+
 tap.test('render', (t) => {
   const pr = new PagedataRenderer('apiKey', { path: __dirname });
   pr.render(templateFile, { content: { text: 'Hello World' } }, (err, result) => {
@@ -94,6 +179,57 @@ tap.test('renderCollection', (t) => {
   }, t.end);
 });
 
+tap.test('renderCollection with common', (t) => {
+  const templateFile2 = path.join(__dirname, 'fixture', 'commonPage.njk');
+  const pr = new PagedataRenderer('apiKey', {
+    path: __dirname,
+    host: 'http://localhost:8081',
+    common: {
+      header: 'website-header'
+    }
+  });
+  async.autoInject({
+    server(done) {
+      const server = new Hapi.Server();
+      server.connection({
+        host: 'localhost',
+        port: 8081
+      });
+      server.route({
+        path: '/api/pages',
+        method: 'get',
+        handler(request, reply) {
+          t.equal(request.query.parentPageSlug, 'collection-slug');
+          t.equal(request.query.populate, 'content');
+          return reply(null, [
+            { slug: 'page1-slug', content: { text: 'Hello World 1' } },
+            { slug: 'page2-slug', content: { text: 'Hello World 2' } },
+          ]);
+        }
+      });
+      server.route({
+        path: '/api/pages/website-header',
+        method: 'get',
+        handler(request, reply) {
+          return reply(null, { content: { logo: 'awesome' } });
+        }
+      });
+      server.start(() => done(null, server));
+    },
+    call(server, done) {
+      pr.renderCollection('collection-slug', templateFile2, (err, result) => {
+        t.equal(err, null);
+        t.notEqual(result['page1-slug'].indexOf('awesomeHello World 1'), -1);
+        t.notEqual(result['page2-slug'].indexOf('awesomeHello World 2'), -1);
+        done();
+      });
+    },
+    stop(server, call, done) {
+      server.stop(done);
+    }
+  }, t.end);
+});
+
 tap.test('renderAndSave', (t) => {
   const pr = new PagedataRenderer('apiKey', { path: __dirname, host: 'http://localhost:8081' });
   async.autoInject({
@@ -126,57 +262,6 @@ tap.test('renderAndSave', (t) => {
       t.equal(fs.existsSync(path.join(__dirname, 'output', 'page-two-slug', 'index.html')), true, 'creates index.html in page sub-directory');
       t.equal(fs.existsSync(path.join(__dirname, 'output', 'index.html')), true, 'creates homepage in output/index.html');
       done();
-    },
-    stop(server, call, done) {
-      server.stop(done);
-    }
-  }, t.end);
-});
-
-tap.test('fetch with common', (t) => {
-  const pr = new PagedataRenderer('apiKey', {
-    path: __dirname,
-    host: 'http://localhost:8081',
-    common: {
-      header: 'site-header'
-    }
-  });
-  async.autoInject({
-    server(done) {
-      const server = new Hapi.Server();
-      server.connection({
-        host: 'localhost',
-        port: 8081
-      });
-      server.route({
-        path: '/api/pages/page-slug',
-        method: 'get',
-        handler(request, reply) {
-          return reply(null, { content: { text: 'Hello World' } });
-        }
-      });
-      server.route({
-        path: '/api/pages/site-header',
-        method: 'get',
-        handler(request, reply) {
-          return reply(null, { content: { logo: 'awesome' } });
-        }
-      });
-      server.start(() => done(null, server));
-    },
-    call(server, done) {
-      pr.fetch('page-slug', (err, result) => {
-        t.equal(err, null);
-        t.deepEqual(result, {
-          header: {
-            logo: 'awesome'
-          },
-          content: {
-            text: 'Hello World'
-          }
-        });
-        done();
-      });
     },
     stop(server, call, done) {
       server.stop(done);
